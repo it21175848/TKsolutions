76be127c7e048ba834c8795fbd91558c245cd4d0
it21175848 <it21175848@my.sliit.lk>
2023-10-03 23:59:02 +0530

    change and modify code js

--- hi.js
+++ hi.js
@@ -1,15 +1,17 @@
 function verifyOTP() {
-    const otp = document.getElementById("digit1").value +
-                document.getElementById("digit2").value +
-                document.getElementById("digit3").value +
-                document.getElementById("digit4").value;
+    const digit1 = document.getElementById("digit1").value;
+    const digit2 = document.getElementById("digit2").value;
+    const digit3 = document.getElementById("digit3").value;
+    const digit4 = document.getElementById("digit4").value;
+
+    const otp = digit1 + digit2 + digit3 + digit4;
 
     // Replace this with your actual OTP validation logic
-    const validOTP = "kiru"; // Change this to your expected OTP
+    const validOTP = "1234"; // Change this to your expected OTP
 
     if (otp === validOTP) {
-        alert("OTP Verified!");
+        document.getElementById("message").innerText = "OTP Verified!";
     } else {
-        alert("OTP Invalid. Please try again.");
+        document.getElementById("message").innerText = "OTP Invalid. Please try again.";
     }
 }
